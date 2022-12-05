e50eb5d0f5401cd416d06c79933de3f95008d689
Aniruddha-S-Prasad <aniruddha.asp12@gmail.com>
2022-12-05 18:46:50 +0100

    Day 5 completed

--- 05_supply_stacks/crane.py
+++ 05_supply_stacks/crane.py
@@ -7,12 +7,12 @@ with open('stack.txt', 'r') as init_stack_file:
 crate_arrangement.pop()
 
 crate_arrangement = [
-    [line[2 * ctr + 1] for line in crate_arrangement] for ctr in range(8)
+    [line[2 * ctr + 1] for line in crate_arrangement] for ctr in range(9)
     ]
 
 for line in crate_arrangement:
     line.reverse()
-    while line[-1] == '.' or line[-1] == ' ':
+    while line[-1] == '.' or line[-1] == ' ' or line[-1] == '\n':
         line.pop()
 for line in crate_arrangement:
     print(line)
@@ -26,9 +26,9 @@ instructions = [
 
 for line in instructions:
     x = [int(y) for y in line]
-    print(x[2])
-    move_crates(x[0], crate_arrangement[x[1] - 1], crate_arrangement[x[2] - 1])
-    print(crate_arrangement)
+    # print(f'move {x[0]} from {x[1]} to {x[2]}')
+    move_crates(x[0], crate_arrangement[x[1] - 1], crate_arrangement[x[2] - 1], crane_9001=True)
 
 for line in crate_arrangement:
-    print(line)
+    print(line[-1], end='')
+print()
--- 05_supply_stacks/crate_mover.py
+++ 05_supply_stacks/crate_mover.py
@@ -1,4 +1,12 @@
-def move_crates(num_crates: int, from_place: list, to_place: list):
-    while num_crates > 0:
-        to_place.append(from_place.pop())
-        num_crates -= 1
+def move_crates(num_crates: int, from_place: list, to_place: list, crane_9001: bool=False):
+    if (crane_9001):
+        crane_space = []
+        while num_crates > 0:
+            crane_space.append(from_place.pop())
+            num_crates -=1
+        crane_space.reverse()
+        to_place.extend(crane_space)
+    else:
+        while num_crates > 0:
+            to_place.append(from_place.pop())
+            num_crates -= 1

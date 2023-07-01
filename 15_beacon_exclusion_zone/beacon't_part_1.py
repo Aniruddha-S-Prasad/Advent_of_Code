8b9fcff3d7277d192d1bf634df3b15e799632137
Aniruddha S Prasad <aniruddha.asp12@gmail.com>
2023-07-01 22:12:12 +0200

    Day 15: More optimizations

--- 15_beacon_exclusion_zone/beacon't_part_1.py
+++ 15_beacon_exclusion_zone/beacon't_part_1.py
@@ -1,26 +1,22 @@
-from dataclasses import dataclass
+from operator import itemgetter
 from time import time
 
-def manhattan_distance(loc_a: tuple[int, int], loc_b: tuple[int, int]) -> int:
-    dist_x = abs(loc_b[0] - loc_a[0])
-    dist_y = abs(loc_b[1] - loc_a[1])
-    return dist_x + dist_y
-
-
-@dataclass
 class SensorBeaconPair:
-    sensor_loc: tuple[int, int]
-    beacon_loc: tuple[int, int]
-    distance: int = 0
-    
-    def __post_init__(self) -> None:
-        self.distance = manhattan_distance(self.sensor_loc, self.beacon_loc)
+    def __init__(self, sensor_loc: tuple[int, int], beacon_loc: tuple[int, int]) -> None:
+        self.sensor_loc = sensor_loc
+        self.beacon_loc = beacon_loc
+        self.distance = self.manhattan_distance(self.sensor_loc, self.beacon_loc)
 
     def __str__(self) -> str:
         sensor_str = "Sensor Location: " + str(self.sensor_loc)
         beacon_str = "Beacon Location: " + str(self.beacon_loc)
         distance_str = "Manhattan Distance: " + str(self.distance)
         return sensor_str + ' ' + beacon_str + ' ' + distance_str
+    
+    def manhattan_distance(self, loc_a: tuple[int, int], loc_b: tuple[int, int]) -> int:
+        dist_x = abs(loc_b[0] - loc_a[0])
+        dist_y = abs(loc_b[1] - loc_a[1])
+        return dist_x + dist_y
 
 
 def parse_location(loc_str: str) -> tuple[int, int]:
@@ -28,20 +24,18 @@ def parse_location(loc_str: str) -> tuple[int, int]:
     return location
 
 
-def resolve_coverage(coverage_desc: list[int]) -> list[int]:
-    for _ in range(2):
-        to_be_deleted = []
-        for i in range(len(coverage_desc)):
-            for j in range(i + 1, len(coverage_desc)):
-                if (coverage_desc[j][0] <= coverage_desc[i][0] <= coverage_desc[j][1] + 1) or (coverage_desc[j][0] - 1 <= coverage_desc[i][1] <= coverage_desc[j][1]):
-                        coverage_desc[j][0] = min([coverage_desc[i][0], coverage_desc[j][0]])
-                        coverage_desc[j][1] = max([coverage_desc[i][1], coverage_desc[j][1]])
-                        to_be_deleted.append(i)
-                        break
-        for index in sorted(to_be_deleted, reverse=True):
-            del coverage_desc[index]
-        coverage_desc.reverse()
-    return
+def resolve_coverage(coverage_desc: list[tuple[int, int]]) -> list[tuple[int, int]]:
+    coverage_desc.sort(key=itemgetter(0, 1))
+    coverage = []
+    current_range = list(coverage_desc[0])
+    for coverage_item in coverage_desc:
+        if (current_range[0] <= coverage_item[0] <= current_range[1] + 1):
+            current_range[1] = coverage_item[1] if coverage_item[1] > current_range[1] else current_range[1]
+        else:
+            coverage.append((current_range[0], current_range[1]))
+            current_range = list(coverage_item)
+    coverage.append((current_range[0], current_range[1]))
+    return coverage
 
 
 def main(loc_data: list[str], y_check: int):
@@ -56,9 +50,9 @@ def main(loc_data: list[str], y_check: int):
             available_x = sensor.distance - y_dist
             left_lim = sensor.sensor_loc[0] - available_x
             right_lim = sensor.sensor_loc[0] + available_x
-            coverage.append([left_lim, right_lim])
-    resolve_coverage(coverage)
-
+            coverage.append((left_lim, right_lim))
+    coverage = resolve_coverage(coverage)
+    
     num_devices_in_row = 0
     for location in occupied_locs:
         if location[1] == y_check:
--- 15_beacon_exclusion_zone/beacon't_part_2.py
+++ 15_beacon_exclusion_zone/beacon't_part_2.py
@@ -1,27 +1,23 @@
-
-from dataclasses import dataclass
+from operator import itemgetter, attrgetter
 from time import time
 
-def manhattan_distance(loc_a: tuple[int, int], loc_b: tuple[int, int]) -> int:
-    dist_x = abs(loc_b[0] - loc_a[0])
-    dist_y = abs(loc_b[1] - loc_a[1])
-    return dist_x + dist_y
 
-
-@dataclass
 class SensorBeaconPair:
-    sensor_loc: tuple[int, int]
-    beacon_loc: tuple[int, int]
-    distance: int = 0
-    
-    def __post_init__(self) -> None:
-        self.distance = manhattan_distance(self.sensor_loc, self.beacon_loc)
+    def __init__(self, sensor_loc: tuple[int, int], beacon_loc: tuple[int, int]) -> None:
+        self.sensor_loc = sensor_loc
+        self.beacon_loc = beacon_loc
+        self.distance = self.manhattan_distance(self.sensor_loc, self.beacon_loc)
 
     def __str__(self) -> str:
         sensor_str = "Sensor Location: " + str(self.sensor_loc)
         beacon_str = "Beacon Location: " + str(self.beacon_loc)
         distance_str = "Manhattan Distance: " + str(self.distance)
         return sensor_str + ' ' + beacon_str + ' ' + distance_str
+    
+    def manhattan_distance(self, loc_a: tuple[int, int], loc_b: tuple[int, int]) -> int:
+        dist_x = abs(loc_b[0] - loc_a[0])
+        dist_y = abs(loc_b[1] - loc_a[1])
+        return dist_x + dist_y
 
 
 def parse_location(loc_str: str) -> tuple[int, int]:
@@ -29,39 +25,50 @@ def parse_location(loc_str: str) -> tuple[int, int]:
     return location
 
 
-def resolve_coverage(coverage_desc: list[int]) -> list[int]:
-    for _ in range(2):
-        to_be_deleted = []
-        for i in range(len(coverage_desc)):
-            for j in range(i + 1, len(coverage_desc)):
-                if (coverage_desc[j][0] <= coverage_desc[i][0] <= coverage_desc[j][1] + 1) or (coverage_desc[j][0] - 1 <= coverage_desc[i][1] <= coverage_desc[j][1]):
-                        coverage_desc[j][0] = min([coverage_desc[i][0], coverage_desc[j][0]])
-                        coverage_desc[j][1] = max([coverage_desc[i][1], coverage_desc[j][1]])
-                        to_be_deleted.append(i)
-                        break
-        for index in sorted(to_be_deleted, reverse=True):
-            del coverage_desc[index]
-        coverage_desc.reverse()
-    return
+def resolve_coverage(coverage_desc: list[tuple[int, int]]) -> list[tuple[int, int]]:
+    coverage_desc.sort(key=itemgetter(0, 1))
+    coverage = []
+    current_range = list(coverage_desc[0])
+    for coverage_item in coverage_desc:
+        if (current_range[0] <= coverage_item[0] <= current_range[1] + 1):
+            current_range[1] = coverage_item[1] if coverage_item[1] > current_range[1] else current_range[1]
+        else:
+            coverage.append((current_range[0], current_range[1]))
+            current_range = list(coverage_item)
+    coverage.append((current_range[0], current_range[1]))
+    return coverage
 
 
 def main(loc_data: list[str], search_size: int):
     sb_pairs = [SensorBeaconPair(*list(map(parse_location, line.split(':')))) for line in loc_data]
+    sb_pairs.sort(key=lambda sensor: sensor.sensor_loc[0])
+    search_size_10_percent = search_size//10
 
     loop_start = time()
     for y_check in range(search_size):
         coverage = []
-
+        initial = True
         for sensor in sb_pairs:
             y_dist = abs(sensor.sensor_loc[1] - y_check)
             if y_dist < sensor.distance:
                 available_x = sensor.distance - y_dist
                 left_lim = sensor.sensor_loc[0] - available_x
                 right_lim = sensor.sensor_loc[0] + available_x
-                coverage.append([left_lim if left_lim > 0 else 0, right_lim if right_lim < search_size else search_size])
-        resolve_coverage(coverage)
-        
-        if (y_check % 400_000 == 0):
+                coverage_item = (left_lim if left_lim > 0 else 0, right_lim if right_lim < search_size else search_size)
+
+                if initial:
+                    current_range = list(coverage_item)
+                    initial = False
+                elif (current_range[0] <= coverage_item[0] <= current_range[1] + 1):
+                    current_range[1] = coverage_item[1] if coverage_item[1] > current_range[1] else current_range[1]
+                else:
+                    coverage.append((current_range[0], current_range[1]))
+                    current_range = list(coverage_item)
+
+        coverage.append((current_range[0], current_range[1]))
+        coverage = resolve_coverage(coverage)
+
+        if (y_check % search_size_10_percent == 0):
             print(f"Completion: {y_check*100/search_size}") 
 
         if (len(coverage) > 1):

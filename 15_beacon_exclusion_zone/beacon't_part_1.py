5891e53322d5170a72b1b03f70c2283e8e2dae14
Aniruddha S Prasad <aniruddha.asp12@gmail.com>
2023-07-01 14:48:19 +0200

    Day 15: Part 1 even more optimised

--- 15_beacon_exclusion_zone/beacon't_part_1.py
+++ 15_beacon_exclusion_zone/beacon't_part_1.py
@@ -1,4 +1,3 @@
-import numpy as np
 from dataclasses import dataclass
 from time import time
 
@@ -29,28 +28,59 @@ def parse_location(loc_str: str) -> tuple[int, int]:
     return location
 
 
+def coverage_union(coverage_desc: list[list[int]], addn_coverage: list[int]) -> None:
+    item_added = False
+    if len(coverage_desc) != 0:
+        for coverage_item in coverage_desc:
+            if (coverage_item[0] <= addn_coverage[0] <= coverage_item[1]) or (coverage_item[0] <= addn_coverage[1] <= coverage_item[1]):
+                coverage_item[0] = min([coverage_item[0], addn_coverage[0]])
+                coverage_item[1] = max([coverage_item[1], addn_coverage[1]])
+                item_added = True
+                break
+        if not item_added:
+            coverage_desc.append(addn_coverage)
+    else:
+        coverage_desc.append(addn_coverage)
+
+
+def resolve_coverage(coverage_desc: list[int]) -> list[int]:
+    to_be_deleted = []
+    for i in range(len(coverage_desc)):
+        for j in range(i+1, len(coverage_desc)):
+            if (coverage_desc[j][0] <= coverage_desc[i][0] <= coverage_desc[j][1]) or (coverage_desc[j][0] <= coverage_desc[i][1] <= coverage_desc[j][1]):
+                    coverage_desc[j][0] = min([coverage_desc[i][0], coverage_desc[j][0]])
+                    coverage_desc[j][1] = max([coverage_desc[i][1], coverage_desc[j][1]])
+                    to_be_deleted.append(i)
+                    break
+    for index in sorted(to_be_deleted, reverse=True):
+        del coverage_desc[index]
+    pass
+
+
 def main(loc_data: list[str], y_check: int):
     sb_pairs = [SensorBeaconPair(*list(map(parse_location, line.split(':')))) for line in loc_data]
     occupied_locs = set([x.sensor_loc for x in sb_pairs] + [x.beacon_loc for x in sb_pairs])
 
     x_min = min([pair.sensor_loc[0] - pair.distance for pair in sb_pairs])
     x_max = max([pair.sensor_loc[0] + pair.distance for pair in sb_pairs])
-    x_row = np.zeros(x_max-x_min, dtype=np.int32)
+    coverage = []
 
     loop_start = time()
     for sensor in sb_pairs:
         y_dist = abs(sensor.sensor_loc[1] - y_check)
         if y_dist < sensor.distance:
             available_x = sensor.distance - y_dist
-            left_lim = sensor.sensor_loc[0] - available_x - x_min
-            right_lim = sensor.sensor_loc[0] + available_x - x_min
-            x_row[left_lim:right_lim+1] = 1
+            left_lim = sensor.sensor_loc[0] - available_x
+            right_lim = sensor.sensor_loc[0] + available_x
+            coverage_union(coverage, [left_lim, right_lim])
+    resolve_coverage(coverage)
 
+    num_devices_in_row = 0
     for location in occupied_locs:
-        if (location[1] == y_check):
-            x_row[location[0]-x_min] = 0
+        if location[1] == y_check:
+            num_devices_in_row +=1
     
-    num_covered_locs = np.sum(x_row)
+    num_covered_locs = sum([coverage_item[1] + 1 - coverage_item[0] for coverage_item in coverage]) - num_devices_in_row
     loop_end = time()
     print(f'Covered locations in row {y_check} is: {num_covered_locs}')
     print('Time in loop:')
